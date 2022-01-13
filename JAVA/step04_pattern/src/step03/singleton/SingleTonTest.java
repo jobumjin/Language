ad37a3b7ca5911785016a469f3f8d0a99cece81a
JoBumJin <whqjawls2673@naver.com>
2022-01-13 18:00:47 +0900

    JavaProgramming_08

---
+++ JAVA/step04_pattern/src/step03/singleton/SingleTonTest.java
@@ -0,0 +1,24 @@
+/* 학습 내용
+ * singleton design pattern
+ * 
+ * 1. server의 물리적인 자원의 절약측면에서 필요한 메커니즘
+ * 2. server 내부의 실행시에 객체수는 무조건 하나만 생성 보장
+ * 3. 개발 방식
+ * 		1. 객체 생성 금지 - 외부에서 객체 생성금지
+ * 			private 생성자(){}
+ * 		2. 외부에서 이 객체의 다른 기능(메소드)들 활용 방법
+ * 			1. 해당 클래스 내에서 객체 생성
+ * 			2. 생성된 객체를 메소드로 반환
+ */
+package step03.singleton;
+
+public class SingleTonTest {
+
+	public static void main(String[] args) {
+		
+		SingleTon s = SingleTon.getInstance();
+		//SingleTon의 메소드 사용해야하는 로직
+		//s. ...
+	}
+
+}
